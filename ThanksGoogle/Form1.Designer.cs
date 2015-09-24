5541e748cd51924c166d3834d29d50f11aa7be08
拉狗那 <lag945@hotmail.com>
2015-09-24 10:03:43 +0800

    ver 1.0

---
+++ ThanksGoogle/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace ThanksGoogle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBX_West = new System.Windows.Forms.TextBox();
+            this.TBX_North = new System.Windows.Forms.TextBox();
+            this.TBX_East = new System.Windows.Forms.TextBox();
+            this.TBX_South = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LBL_TolalPages = new System.Windows.Forms.Label();
+            this.LBL_Process = new System.Windows.Forms.Label();
+            this.TBX_ExportPath = new System.Windows.Forms.TextBox();
+            this.BTN_ExportPath = new System.Windows.Forms.Button();
+            this.BTN_Start = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TBX_ServerUrl = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(43, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "精細度(越大越細):";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(281, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(87, 20);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "影像類型:";
+            // 
+            // TBX_West
+            // 
+            this.TBX_West.Location = new System.Drawing.Point(21, 40);
+            this.TBX_West.Name = "TBX_West";
+            this.TBX_West.Size = new System.Drawing.Size(100, 22);
+            this.TBX_West.TabIndex = 4;
+            this.TBX_West.Text = "119";
+            this.TBX_West.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TBX_North
+            // 
+            this.TBX_North.Location = new System.Drawing.Point(127, 27);
+            this.TBX_North.Name = "TBX_North";
+            this.TBX_North.Size = new System.Drawing.Size(100, 22);
+            this.TBX_North.TabIndex = 5;
+            this.TBX_North.Text = "26";
+            this.TBX_North.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TBX_East
+            // 
+            this.TBX_East.Location = new System.Drawing.Point(233, 40);
+            this.TBX_East.Name = "TBX_East";
+            this.TBX_East.Size = new System.Drawing.Size(100, 22);
+            this.TBX_East.TabIndex = 6;
+            this.TBX_East.Text = "122";
+            this.TBX_East.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TBX_South
+            // 
+            this.TBX_South.Location = new System.Drawing.Point(127, 54);
+            this.TBX_South.Name = "TBX_South";
+            this.TBX_South.Size = new System.Drawing.Size(100, 22);
+            this.TBX_South.TabIndex = 7;
+            this.TBX_South.Text = "21";
+            this.TBX_South.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.TBX_West);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.TBX_North);
+            this.groupBox1.Controls.Add(this.TBX_South);
+            this.groupBox1.Controls.Add(this.TBX_East);
+            this.groupBox1.Location = new System.Drawing.Point(14, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 98);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "範圍(WGS84經緯)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(167, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "下";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(337, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "右";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "上";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "左";
+            // 
+            // LBL_TolalPages
+            // 
+            this.LBL_TolalPages.AutoSize = true;
+            this.LBL_TolalPages.Location = new System.Drawing.Point(12, 247);
+            this.LBL_TolalPages.Name = "LBL_TolalPages";
+            this.LBL_TolalPages.Size = new System.Drawing.Size(68, 12);
+            this.LBL_TolalPages.TabIndex = 9;
+            this.LBL_TolalPages.Text = "預估總張數:";
+            // 
+            // LBL_Process
+            // 
+            this.LBL_Process.AutoSize = true;
+            this.LBL_Process.Location = new System.Drawing.Point(12, 271);
+            this.LBL_Process.Name = "LBL_Process";
+            this.LBL_Process.Size = new System.Drawing.Size(56, 12);
+            this.LBL_Process.TabIndex = 10;
+            this.LBL_Process.Text = "剩餘張數:";
+            // 
+            // TBX_ExportPath
+            // 
+            this.TBX_ExportPath.Location = new System.Drawing.Point(71, 153);
+            this.TBX_ExportPath.Name = "TBX_ExportPath";
+            this.TBX_ExportPath.Size = new System.Drawing.Size(221, 22);
+            this.TBX_ExportPath.TabIndex = 11;
+            // 
+            // BTN_ExportPath
+            // 
+            this.BTN_ExportPath.Location = new System.Drawing.Point(298, 152);
+            this.BTN_ExportPath.Name = "BTN_ExportPath";
+            this.BTN_ExportPath.Size = new System.Drawing.Size(75, 23);
+            this.BTN_ExportPath.TabIndex = 12;
+            this.BTN_ExportPath.Text = "瀏覽...";
+            this.BTN_ExportPath.UseVisualStyleBackColor = true;
+            this.BTN_ExportPath.Click += new System.EventHandler(this.BTN_ExportPath_Click);
+            // 
+            // BTN_Start
+            // 
+            this.BTN_Start.Location = new System.Drawing.Point(298, 260);
+            this.BTN_Start.Name = "BTN_Start";
+            this.BTN_Start.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Start.TabIndex = 13;
+            this.BTN_Start.Text = "開始";
+            this.BTN_Start.UseVisualStyleBackColor = true;
+            this.BTN_Start.Click += new System.EventHandler(this.BTN_Start_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 12);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "輸出目錄:";
+            // 
+            // TBX_ServerUrl
+            // 
+            this.TBX_ServerUrl.Location = new System.Drawing.Point(71, 182);
+            this.TBX_ServerUrl.Name = "TBX_ServerUrl";
+            this.TBX_ServerUrl.Size = new System.Drawing.Size(221, 22);
+            this.TBX_ServerUrl.TabIndex = 15;
+            this.TBX_ServerUrl.Text = "http://127.0.0.1:8080";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 185);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 12);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "轉座標:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 295);
+            this.Controls.Add(this.TBX_ServerUrl);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.BTN_Start);
+            this.Controls.Add(this.BTN_ExportPath);
+            this.Controls.Add(this.TBX_ExportPath);
+            this.Controls.Add(this.LBL_Process);
+            this.Controls.Add(this.LBL_TolalPages);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "感謝谷歌";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBX_West;
+        private System.Windows.Forms.TextBox TBX_North;
+        private System.Windows.Forms.TextBox TBX_East;
+        private System.Windows.Forms.TextBox TBX_South;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LBL_TolalPages;
+        private System.Windows.Forms.Label LBL_Process;
+        private System.Windows.Forms.TextBox TBX_ExportPath;
+        private System.Windows.Forms.Button BTN_ExportPath;
+        private System.Windows.Forms.Button BTN_Start;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox TBX_ServerUrl;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ThanksGoogle/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ThanksGoogle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] list = new string[19];
+            for (int i = 0; i <= 18; i++) list[i] = i.ToString();
+
+            comboBox1.Items.AddRange(list);
+            comboBox1.SelectedIndex = 10;
+
+            comboBox2.Items.AddRange(new string[] { "衛星", "向量", "衛星+向量" });
+            comboBox2.SelectedIndex = 0;
+
+            TBX_ExportPath.Text = Application.StartupPath;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_ThreadList != null)
+            {
+                for (int i = 0; i < ThreadSize; i++)
+                {
+                    if (m_ThreadList[i] != null) m_ThreadList[i].Abort();
+                }
+            }
+        }
+
+
+        private void BTN_ExportPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog(this) == DialogResult.OK)
+            {
+                TBX_ExportPath.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void BTN_Start_Click(object sender, EventArgs e)
+        {
+            z = comboBox1.SelectedIndex;
+            if (z < 0)
+            {
+                MessageBox.Show("請選擇精細度", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (comboBox2.SelectedIndex < 0)
+            {
+                MessageBox.Show("請選擇影像類型", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double west, north, east, south;
+            if (!double.TryParse(TBX_West.Text, out west))
+            {
+                MessageBox.Show("西邊範圍輸入錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(TBX_North.Text, out north))
+            {
+                MessageBox.Show("北邊範圍輸入錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(TBX_East.Text, out east))
+            {
+                MessageBox.Show("東邊範圍輸入錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(TBX_South.Text, out south))
+            {
+                MessageBox.Show("南邊範圍輸入錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Directory.Exists(TBX_ExportPath.Text))
+            {
+                try
+                {
+                    Directory.CreateDirectory(TBX_ExportPath.Text);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                if (!Directory.Exists(TBX_ExportPath.Text))
+                {
+                    MessageBox.Show("輸出目錄創建失敗", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            ExportPath = TBX_ExportPath.Text;
+
+            try
+            {
+                string url = TBX_ServerUrl.Text + "/docmd?cmd=EPSG_Transfer&FromEPSG=4326&ToEPSG=3857&Points=";
+                url += TBX_West.Text + "," + TBX_North.Text;
+                url += "," + TBX_East.Text + "," + TBX_North.Text;
+                url += "," + TBX_East.Text + "," + TBX_South.Text;
+                url += "," + TBX_West.Text + "," + TBX_North.Text;
+
+                HttpWebRequest req = (HttpWebRequest)(WebRequest.Create(url));
+                req.Method = "GET";
+                req.UserAgent = "MyBrowser";
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                Stream stream = res.GetResponseStream();
+                MemoryStream ms = new MemoryStream();
+                int len = 1024 * 1024;
+                byte[] bytes = new byte[len];
+                int readlen = 0;
+                do
+                {
+                    readlen = stream.Read(bytes, 0, len);
+                    ms.Write(bytes, 0, readlen);
+                }
+                while (readlen > 0);
+
+                string s = Encoding.UTF8.GetString(ms.ToArray());
+                int startindex = 0;
+                int a = -1, b = -1;
+                List<string> strs = new List<string>();
+                while (true)
+                {
+                    a = s.IndexOf("\"coordinates\": [", startindex);
+                    if (a >= 0) a += 16;
+                    else break;
+
+                    startindex = a;
+                    b = s.IndexOf("]}", startindex);
+                    startindex = b;
+
+                    if (a >= 0 && b >= 0)
+                    {
+                        string _s = s.Substring(a, b - a);
+                        strs.Add(_s);
+                    }
+                    else break;
+                }
+
+                for (int i = 0; i < strs.Count; i++)
+                {
+                    string[] ss = strs[i].Split(',');
+                    double x = Convert.ToDouble(ss[0]);
+                    double y = Convert.ToDouble(ss[1]);
+                    if (i == 0)
+                    {
+                        west = east = x;
+                        south = north = y;
+                    }
+                    else
+                    {
+                        if (x < west) west = x;
+                        else if (x > east) east = x;
+
+                        if (y < south) south = y;
+                        else if (y > north) north = y;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("轉換座標失敗" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //開始打格子
+            width = 20037508.34278925 * 2 / Math.Pow(2, z);
+            minx = Convert.ToInt32(Math.Floor((west - left) / width));
+            miny = Convert.ToInt32(Math.Floor((top - north) / width));
+            maxx = Convert.ToInt32(Math.Ceiling((east - left) / width));
+            maxy = Convert.ToInt32(Math.Ceiling((top - south) / width));
+
+            if (minx <= 0) minx = 0;
+            if (maxx <= 0) maxx = 0;
+            if (miny <= 0) miny = 0;
+            if (maxy <= 0) maxy = 0;
+            if (minx >= Math.Pow(2, z)) minx = Convert.ToInt32(Math.Pow(2, z)) - 1;
+            if (maxx >= Math.Pow(2, z)) maxx = Convert.ToInt32(Math.Pow(2, z)) - 1;
+            if (miny >= Math.Pow(2, z)) miny = Convert.ToInt32(Math.Pow(2, z)) - 1;
+            if (maxy >= Math.Pow(2, z)) maxy = Convert.ToInt32(Math.Pow(2, z)) - 1;
+
+            imagetype = comboBox2.SelectedIndex;
+            ErrorCount = 0;
+
+            Console.WriteLine(minx.ToString() + " " + maxx.ToString() + " " + miny.ToString() + " " + maxy.ToString());
+
+            m_TotalCount = ((maxx - minx + 1) * (maxy - miny + 1));
+            LBL_TolalPages.Text = "預估總張數:" + m_TotalCount.ToString("N0");
+
+            Thread thd = new Thread(MainThreadWork);
+            LBL_Process.Text = "剩餘張數:" + "計算中...";
+            BTN_Start.Enabled = false;
+            thd.Start();
+        }
+
+        int ThreadSize = 12;
+        int LiveThread = 0;
+
+        double z = 0;
+        int imagetype = 0;
+        double left = -20037508.34278925;
+        double top = 20037508.34278925;
+        double width = 20037508.34278925 * 2;
+        int minx = 0, miny = 0, maxx = 0, maxy = 0;
+        int m_TotalCount = 0;
+        string ExportPath = "";
+        List<int> m_xList = new List<int>();
+        List<int> m_yList = new List<int>();
+        Thread[] m_ThreadList = null;
+        int ErrorCount = 0;
+        Random m_Rnd = new Random();
+
+        public void MainThreadWork()
+        {
+            m_xList.Clear();
+            m_xList.Capacity = m_TotalCount;
+            m_yList.Clear();
+            m_yList.Capacity = m_TotalCount;
+            for (int y = miny; y <= maxy; y++)
+            {
+                for (int x = minx; x <= maxx; x++)
+                {
+                    m_xList.Add(x);
+                    m_yList.Add(y);
+                }
+            }
+
+            m_ThreadList = new Thread[ThreadSize];
+            for (int i = 0; i < ThreadSize; i++)
+            {
+                m_ThreadList[i] = new Thread(SubThreadWork);
+                LiveThread++;
+                m_ThreadList[i].Start();
+            }
+        }
+
+        public void SubThreadWork()
+        {
+            while (true)
+            {
+                int x = -1, y = -1;
+                lock (m_xList)
+                {
+                    lock (m_yList)
+                    {
+                        if (m_xList.Count > 0 && m_yList.Count > 0)
+                        {
+                            x = m_xList[m_xList.Count - 1];
+                            y = m_yList[m_yList.Count - 1];
+                            m_xList.RemoveAt(m_xList.Count - 1);
+                            m_yList.RemoveAt(m_yList.Count - 1);
+                        }
+                        else break;
+                    }
+                }
+                if (x >= 0 && y >= 0)
+                {
+                    bool bOK = false;
+                    if (MakeImageFile(x, y))
+                    {
+                        if (MakeCoordinateFile(x, y))
+                        {
+                            bOK = true;
+                            UIText("剩餘張數:" + m_xList.Count.ToString(), LBL_Process);
+                        }
+                    }
+                    if (!bOK) ErrorCount++;
+                }
+            }
+            if (m_ThreadList != null)
+            {
+                for (int i = 0; i < ThreadSize; i++)
+                {
+                    if (m_ThreadList[i] == Thread.CurrentThread)
+                    {
+                        m_ThreadList[i] = null;
+                        break;
+                    }
+                }
+            }
+            LiveThread--;
+            if (LiveThread == 0)
+            {
+                MessageBox.Show("完成" + ((ErrorCount > 0) ? "，" + ErrorCount.ToString() + " 失敗。" : ""), "資訊", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                UIEnabled(true, BTN_Start);
+            }
+        }
+
+        public bool MakeImageFile(int x, int y)
+        {
+            bool Ret = false;
+
+            string s = "";
+            if (imagetype == 0) s = string.Format("http://mt{0}.google.com/vt/lyrs=s&hl=zh-TW&x={1}&y={2}&z={3}", m_Rnd.Next(6), x, y, z);
+            else if (imagetype == 1) s = string.Format("http://mt{0}.google.com/vt/lyrs=m&hl=zh-TW&x={1}&y={2}&z={3}", m_Rnd.Next(6), x, y, z);
+            else if (imagetype == 2) s = string.Format("http://mt{0}.google.com/vt/lyrs=y&hl=zh-TW&x={1}&y={2}&z={3}", m_Rnd.Next(6), x, y, z);
+
+            try
+            {
+                string dir = ExportPath + "\\" + z.ToString() + "\\" + y.ToString();
+                if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+                string filename = dir + "\\" + x.ToString() + ".jpg";
+                HttpWebRequest req = (HttpWebRequest)(WebRequest.Create(s));
+                req.Method = "GET";
+                req.UserAgent = "MyBrowser";
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                Stream stream = res.GetResponseStream();
+                Image img = Image.FromStream(stream);
+                img.Save(filename);
+                Ret = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Ret;
+        }
+
+        public bool MakeCoordinateFile(int x, int y)
+        {
+            bool Ret = false;
+
+            try
+            {
+                string dir = ExportPath + "\\" + z.ToString() + "\\" + y.ToString();
+                if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+                string filename = dir + "\\" + x.ToString() + ".jgw";
+                StreamWriter sw = new StreamWriter(filename, false, Encoding.ASCII);
+                sw.WriteLine((width / 256.0).ToString("0.000000"));
+                sw.WriteLine("0.000000");
+                sw.WriteLine("0.000000");
+                sw.WriteLine((width / 256.0 * -1.0).ToString("0.000000"));
+                sw.WriteLine((left + (x * width)).ToString("0.000000"));
+                sw.WriteLine((top - (y * width)).ToString("0.000000"));
+                sw.Close();
+                Ret = true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Ret;
+        }    
+
+        private delegate void UICallBackText(string myStr, Control ctl);
+        private delegate void UICallBackEnabled(bool myBool, Control ctl);
+        private void UIText(string myStr, Control ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                UICallBackText myUpdate = new UICallBackText(UIText);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.Text = myStr;
+            }
+        }
+
+        private void UIEnabled(bool myBool, Control ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                UICallBackEnabled myUpdate = new UICallBackEnabled(UIEnabled);
+                this.Invoke(myUpdate, myBool, ctl);
+            }
+            else
+            {
+                ctl.Enabled = myBool;
+            }
+        }
+
+
+    }
+}
